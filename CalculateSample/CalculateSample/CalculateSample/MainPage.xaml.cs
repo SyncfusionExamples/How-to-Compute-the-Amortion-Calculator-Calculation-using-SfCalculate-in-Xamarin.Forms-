a98e7d707166bcb3d85541a7d5300d3201f4387e
Mohanraj-Guna <45057763+Mohanraj-Guna@users.noreply.github.com>
2019-03-22 16:52:59 +0530

    Add files via upload

---
+++ CalculateSample/CalculateSample/CalculateSample/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Syncfusion.Calculate;
+using Syncfusion.XForms.DataForm;
+using Syncfusion.XForms.DataForm.Editors;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CalculateSample
+{
+    public partial class MainPage : ContentPage
+    {
+        PaymentInfo paymentInfo;
+        CalcQuickBase calcQuick;
+        CalcEngine engine;    
+        public MainPage()
+        {
+            InitializeComponent();
+
+            paymentInfo = new PaymentInfo();
+            calcQuick = new CalcQuickBase();
+            engine = new CalcEngine(calcQuick);
+
+            dataForm.DataObject = paymentInfo;                   
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            string formula = "=-PMT(" + paymentInfo.InterestRate.ToString() + "/" + paymentInfo.NumberOfPaymentPerYear.ToString() + "," + paymentInfo.LoanYears.ToString() + "*" + paymentInfo.NumberOfPaymentPerYear.ToString() + "," + paymentInfo.LoanAmount.ToString() + ")";
+            displayButton.Text = engine.ParseAndComputeFormula(formula);
+        }
+    }
+}
---
+++ CalculateSample/CalculateSample/CalculateSample/PaymentInfo.cs
@@ -0,0 +1,69 @@
+﻿using Syncfusion.XForms.DataForm;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace CalculateSample
+{
+    public class PaymentInfo : INotifyPropertyChanged
+    {
+        private double interestRate;
+        [Display(Name = "Interest Rate")]
+        public double InterestRate
+        {
+            get { return interestRate; }
+            set
+            {
+                interestRate = value;
+                OnPropertyChanged("InterestRate");
+            }
+        }
+
+        private int numberOfPaymentPerYear;
+        [Display(Name = "Number of payments per year")]
+        public int NumberOfPaymentPerYear
+        {
+            get { return numberOfPaymentPerYear; }
+            set
+            {
+                numberOfPaymentPerYear = value;
+                OnPropertyChanged("NumberOfPaymentPerYear");
+
+            }
+        }
+
+        private int loanYears;
+        [Display(Name = "Loan Years")]
+        public int LoanYears
+        {
+            get { return loanYears; }
+            set
+            {
+                loanYears = value;
+
+                OnPropertyChanged("LoanYears");
+
+            }
+        }
+
+        private int loanAmount;
+        [Display(Name = "Loan Amount")]
+        public int LoanAmount
+        {
+            get { return loanAmount; }
+            set
+            {
+                loanAmount = value;
+                OnPropertyChanged("LoanAmount");
+            }
+        }
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
